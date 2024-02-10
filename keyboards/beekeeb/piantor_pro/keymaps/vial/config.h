f84d5791a409113b4b4cfdec058ca2a758999943
Catalin Climov <climov@gmail.com>
2024-02-10 08:49:04 +0200

    Added Vial support for Beekeeb's Piantor Pro

---
+++ keyboards/beekeeb/piantor_pro/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* keyboard uid */
+#define VIAL_KEYBOARD_UID {0x72, 0xD1, 0xB2, 0x31, 0x83, 0x84, 0x13, 0xDE}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
